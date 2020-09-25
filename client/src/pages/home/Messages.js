f15cd251b08c2dd082018b640e4ed0a7e3e4a931
Ali <mojtabavi99@yahoo.com>
2020-09-25 16:58:13 +0330

    styling the messages

--- client/src/pages/home/Messages.js
+++ client/src/pages/home/Messages.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, {Fragment, useEffect} from "react";
 import {Col} from "react-bootstrap";
 import { gql, useLazyQuery } from "@apollo/client";
 import { useMessageDispatch, useMessageState } from '../../context/message';
+import Message from "./Message";
 
 const GET_MESSAGES = gql`
     query getMessages($from: String!){
@@ -50,15 +51,22 @@ export default function Messages(){
      } else if(messagesLoading){
          selectedChatMarkup = <p>Loading ...</p>
      }else if(messages.length > 0){
-         selectedChatMarkup = messages.map((message) => (
-             <p key={message.uuid}>{message.content}</p>
+         selectedChatMarkup = messages.map((message,index) => (
+             <Fragment key={message.uuid}>
+             <Message message={message} />
+                 {index === message.length -1 && (
+                     <div className="invisible">
+                         <hr className="m-0" />
+                     </div>
+                 )}
+             </Fragment>
          ))
      }else if(messages.length === 0){
          selectedChatMarkup = <p>You are now connected!</p>
      }
 
     return (
-        <Col xs={8}>
+        <Col xs={9} className="messages-box d-flex flex-column-reverse">
             {selectedChatMarkup}
         </Col>
     )
